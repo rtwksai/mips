9843c4e8cc35cf38a3bfc4ffd76596c10c771990
Kartik-Sama <sai.karthik@iiitb.org>
2019-11-29 00:27:32 +0530

    modified:   decode.v 	modified:   mipsProcessor.v 	modified:   writeBack.v

--- decode.v
+++ decode.v
@@ -1,9 +1,10 @@
 //Takes input from fetch and parses it and assigns appropriate register values
 
-module decode(instruction, clk, opcode, rs, rt, rd, immediate, shamt, funct, regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram, read_data_1, read_data_2);
+module decode(instruction, clk, opcode, rs, rt, rd, immediate, shamt, funct, regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram, read_data_1, read_data_2,stage2,stage3);
 
 input [31:0]instruction;
 input clk;
+output reg stage2,stage3;
 output reg[5:0]opcode;
 output reg[4:0]rs;
 output reg[4:0]rt;
@@ -48,139 +49,144 @@ initial begin
 end
 
 
-always @(posedge clk or instruction)
-begin 
-    //Parsing the input
-    opcode = instruction[31:26];
-    if(opcode == 6'b000000)
-    begin 
-        flagR = 1;
-        rs = instruction[25:21];
-        rt = instruction[20:16];
-        rd = instruction[15:11];
-        shamt = instruction[10:6];
-        funct = instruction[5:0];
-    end
-    else
-    begin
-        flagR = 0;
-        rs = instruction[25:21];
-        rt = instruction[20:16];
-        immediate = immediate + instruction[15:0];  
-    end
+always @(posedge clk)
+begin
+if(stage2 == 1) begin 
+        //Parsing the input
+        opcode = instruction[31:26];
+        if(opcode == 6'b000000)
+        begin 
+            flagR = 1;
+            rs = instruction[25:21];
+            rt = instruction[20:16];
+            rd = instruction[15:11];
+            shamt = instruction[10:6];
+            funct = instruction[5:0];
+        end
+        else
+        begin
+            flagR = 0;
+            rs = instruction[25:21];
+            rt = instruction[20:16];
+            immediate = immediate + instruction[15:0];  
+        end
 
-    //Passing parsed input into CONTROL
-    //R-Type instruction
-    if(opcode == 6'b000000)
-    begin
-        regDest = 1;
-        branch = 0;
-        memRead = 0;
-        memToReg = 0;
-        aluOP = 2'b1x;
-        memWrite = 0;
-        aluSrc = 0;
-        regWrite = 1;
-    end
-    //Store
-    else if(opcode == 6'b101011)
-    begin
-        regDest = 0;
-        branch = 0;
-        memRead = 0;
-        memToReg = 1'bx; //Dont care taken as 0
-        aluOP = 2'b00;
-        memWrite = 1;
-        aluSrc = 0;
-        regWrite = 0;
-    end
-    //Load 
-    else if(opcode == 6'b100011)
-    begin 
-        regDest = 0;
-        branch = 0;
-        memRead = 1;
-        memToReg = 1; //Dont care taken as 0
-        aluOP = 2'b00;
-        memWrite = 1;
-        aluSrc = 1;
-        regWrite = 1;        
-    end
-    //BEQ
-    else if(opcode == 6'b000100)
-    begin
-        regDest = 0;
-        branch = 1;
-        memRead = 0;
-        memToReg = 1'bx;
-        aluOP = 2'b01;
-        memWrite = 0;
-        aluSrc = 1;
-        regWrite = 0;
-    end
-    //BNE
-    else if(opcode == 6'b000101)
-    begin
-        regDest = 0;
-        branch = 1;
-        memRead = 0;
-        memToReg = 1'bx;
-        aluOP = 2'b01;
-        memWrite = 0;
-        aluSrc = 1;
-        regWrite = 0;
-    end
-    //END
-    else if(opcode == 6'b111111)
-    begin
-        endProgram = 1;
-    end
+        //Passing parsed input into CONTROL
+        //R-Type instruction
+        if(opcode == 6'b000000)
+        begin
+            regDest = 1;
+            branch = 0;
+            memRead = 0;
+            memToReg = 0;
+            aluOP = 2'b1x;
+            memWrite = 0;
+            aluSrc = 0;
+            regWrite = 1;
+        end
+        //Store
+        else if(opcode == 6'b101011)
+        begin
+            regDest = 0;
+            branch = 0;
+            memRead = 0;
+            memToReg = 1'bx; //Dont care taken as 0
+            aluOP = 2'b00;
+            memWrite = 1;
+            aluSrc = 0;
+            regWrite = 0;
+        end
+        //Load 
+        else if(opcode == 6'b100011)
+        begin 
+            regDest = 0;
+            branch = 0;
+            memRead = 1;
+            memToReg = 1; //Dont care taken as 0
+            aluOP = 2'b00;
+            memWrite = 1;
+            aluSrc = 1;
+            regWrite = 1;        
+        end
+        //BEQ
+        else if(opcode == 6'b000100)
+        begin
+            regDest = 0;
+            branch = 1;
+            memRead = 0;
+            memToReg = 1'bx;
+            aluOP = 2'b01;
+            memWrite = 0;
+            aluSrc = 1;
+            regWrite = 0;
+        end
+        //BNE
+        else if(opcode == 6'b000101)
+        begin
+            regDest = 0;
+            branch = 1;
+            memRead = 0;
+            memToReg = 1'bx;
+            aluOP = 2'b01;
+            memWrite = 0;
+            aluSrc = 1;
+            regWrite = 0;
+        end
+        //END
+        else if(opcode == 6'b111111)
+        begin
+            endProgram = 1;
+        end
 
-    //Takes in register addresses and outputs the values in the register to pass it to ALU
-    //Input for reg file read_reg1, read_reg2, write_data and control signal RegDest and outputs are read_data_1 and read_data_2
-    if(regDest == 1)
-    begin
-        write_reg = rd;
-    end
-    else if(regDest == 0)
-    begin
-        write_reg = rt;
-    end
+        //Takes in register addresses and outputs the values in the register to pass it to ALU
+        //Input for reg file read_reg1, read_reg2, write_data and control signal RegDest and outputs are read_data_1 and read_data_2
+        if(regDest == 1)
+        begin
+            write_reg = rd;
+        end
+        else if(regDest == 0)
+        begin
+            write_reg = rt;
+        end
 
-    read_data_1 = register[rs];
-    read_data_2 = register[rt];
+        read_data_1 = register[rs];
+        read_data_2 = register[rt];
 
-    $display("%b", instruction);
-    $display("%b, %b, %b, %b, %b, %b, %b", opcode, rs, rt, rd, shamt, funct, immediate);
-    $display("%b, %b, %b, %b, %b, %b, %b, %b, %b", regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram);
-    $display("%b, %b", read_data_1, read_data_2);
+        $display("%b", instruction);
+        $display("%b, %b, %b, %b, %b, %b, %b", opcode, rs, rt, rd, shamt, funct, immediate);
+        $display("%b, %b, %b, %b, %b, %b, %b, %b, %b", regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram);
+        $display("%b, %b", read_data_1, read_data_2);
+        stage2=0;
+        stage3=1;
+    end
 end
 
 endmodule
 
-//Testbench to check
-module dc_tb;
-    reg [31:0]instruction;
-    wire [5:0]opcode;
-    wire [4:0]rs;
-    wire [4:0]rt;
-    wire [4:0]rd;
-    wire [31:0]immediate;
-    wire [4:0]shamt;
-    wire [5:0]funct;
-    wire regDest;
-    wire branch;
-    wire memRead;
-    wire memToReg;
-    wire [1:0]aluOP;
-    wire memWrite;
-    wire aluSrc;
-    wire regWrite;
-    wire [31:0]read_data_1;
-    wire [31:0]read_data_2;
+// //Testbench to check
+// module dc_tb;
+//     reg [31:0]instruction;
+//     wire [5:0]opcode;
+//     wire [4:0]rs;
+//     wire [4:0]rt;
+//     wire [4:0]rd;
+//     wire [31:0]immediate;
+//     wire [4:0]shamt;
+//     wire [5:0]funct;
+//     wire regDest;
+//     wire branch;
+//     wire memRead;
+//     wire memToReg;
+//     wire [1:0]aluOP;
+//     wire memWrite;
+//     wire aluSrc;
+//     wire regWrite;
+//     wire [31:0]read_data_1;
+//     wire [31:0]read_data_2;
 
-    decode uut(instruction, clk, opcode, rs, rt, rd, immediate, shamt, funct, regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram, read_data_1, read_data_2);
-    initial begin
-        instruction = 32'b10001110000100100000000000000000;
-    end
-endmodule
+//     decode uut(instruction, clk, opcode, rs, rt, rd, immediate, shamt, funct, regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram, read_data_1, read_data_2);
+//     initial begin
+//         instruction = 32'b10001110000100100000000000000000;
+//     end
+
+// endmodule
--- mipsProcessor.v
+++ mipsProcessor.v
@@ -1,11 +1,12 @@
 `include "fetch.v"
+`include "decode.v"
 `include "alu.v"
 `include "writeBack.v"
 `include "memory.v"
 module setSignals(start,stage1,stage2,stage3,stage4,stage5);
     input start;
     output reg stage1,stage2,stage3,stage4,stage5,endProgram;
-    always@(start) begin
+    always@(posedge start) begin
         if(start == 1) begin
             stage1 = 0;
             stage2 = 0;
@@ -28,20 +29,25 @@ module mipsProcessor();
     wire regDest, branch, memRead, memToReg , memWrite, aluSrc, regWrite, zero, endProgram; // control signals
     wire [1:0]aluOP;
     wire [7:0] memaddress;
-    wire [5:0] regaddress;
+    wire [5:0] address1,address2;
     wire [31:0]curInstruction;
     wire [31:0]resvalue;
+    setSignals initialModule(start,stage1,start2,start3,start4,start5);
     fetch fetchModule(pc,start,curInstruction,stage1,stage2,clock);
-    //decode decodeModule(curInstruction,clock,opcode,rs,rt,rd,immediate,shamt,funct,regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram);
+    decode decodeModule(curInstruction,clock,opcode,rs,rt,rd,immediate,shamt,funct,regDest, branch, memRead, memToReg, aluOP, memWrite, aluSrc, regWrite, endProgram,readData1,readData2,stage2,stage3);
     alu aluModule(readData1,readData2,funct,aluOP,immediate,aluSrc,zero,aluOut,stage3,stage4,clock);
     memory memoryModule(memWrite,memRead,memaddress,resvalue,resvalue,stage4,stage5,clock);
-    writeBack writeBackModule(controlWriteBack,regaddress,resvalue,stage5,stage1,clock);
+    writeBack writeBackModule(regWrite,regDest,address1,address2,resvalue,stage5,stage1,clock);
     initial begin
+        $dumpfile("mips.vcd");
+        $dumpvars(0,mipsProcessor);
        pc = 4'b0000;
+       #10;
        start = 1;
        clock = 0; 
     end
-    always@(clock) begin
+    always
+    begin
         start = 0;
         #5;
         clock = ~clock;
